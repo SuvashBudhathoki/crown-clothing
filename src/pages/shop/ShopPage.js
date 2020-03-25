5fa719915160e2800202439b4f1afdde57e36b58
Suvash Budhathoki <SuvashBudhathoki@users.noreply.github.com>
2020-03-25 11:32:23 +1000

    updating with shop page, collection preview and collection item component

---
+++ src/pages/shop/ShopPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import SHOP_DATA from "./shoppingData";
+import CollectionPreview from "../../components/collection-preview/CollectionPreview";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollection }) => (
+          <CollectionPreview key={id} {...otherCollection} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
