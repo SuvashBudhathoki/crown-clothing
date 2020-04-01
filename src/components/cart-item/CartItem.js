20dd6883661ca26c25438a9e8b241673b203da8f
Suvash Budhathoki <SuvashBudhathoki@users.noreply.github.com>
2020-04-01 18:53:45 +1000

    updating our application with reselect, replaced cart selectors with memoized selectors

---
+++ src/components/cart-item/CartItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./cart-item.styles.scss";
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <div className="cart-item">
+    <img src={imageUrl} alt="item" />
+    <div className="item-details">{name}</div>
+    <span className="price">
+      {quantity}x${price}
+    </span>
+  </div>
+);
+
+export default CartItem;
