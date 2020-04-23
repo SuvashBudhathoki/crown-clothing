62517f30727a22e71b4ef902adf57faec66d9a57
Suvash Budhathoki <SuvashBudhathoki@users.noreply.github.com>
2020-04-23 14:41:43 +1000

    updating stripe integration and updating favicon and title

---
+++ src/components/stripe-button/StripeButton.js
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_HD0AQCNyuRIykilCETPp6aJ300cYerBbX1";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing LTD"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
