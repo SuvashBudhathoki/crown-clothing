e22f4d07d2e66ea94511209f97da49813b81a5fe
Suvash Budhathoki <SuvashBudhathoki@users.noreply.github.com>
2020-05-01 19:40:42 +1000

    styled component added for cart-dropdown and cart-icon components

---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const SignInSignUpContainer = styled.div`
+  width: 850px;
+  display: flex;
+  margin: 30px auto;
+  justify-content: space-around;
+`;
